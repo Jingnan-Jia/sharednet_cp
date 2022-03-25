e5b5f22820787dc98169ec16b386c515e020bf22
Jingnan-Jia <jiajingnan2222@gmail.com>
2022-03-25 17:35:23 +0100

    jobid is 202875

--- sharednet/scripts/run.py
+++ sharednet/scripts/run.py
@@ -156,7 +156,7 @@ class Task:
         t3 = time.time()
 
         self.opt.zero_grad()
-        if 0:
+        if 1:
             print('using amp ', end='')
             with torch.cuda.amp.autocast():
                 pred = self.net(image,cond)
