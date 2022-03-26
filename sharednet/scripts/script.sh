8ea0e5a565925a848e5accd7ca30d051be458c18
Jingnan-Jia <jiajingnan2222@gmail.com>
2022-03-26 01:04:43 +0100

    jobid is 202948

--- sharednet/scripts/script.sh
+++ sharednet/scripts/script.sh
@@ -18,7 +18,9 @@ job_id=$SLURM_JOB_ID
 slurm_dir=results/logs
 
 ##cp script.sh ${slurm_dir}/slurm-${job_id}.shs
-#scontrol write batch_script ${job_id} ${slurm_dir}/slurm-${job_id}_args.sh
+# git will not detect the current file because this file may be changed when this job was run
+scontrol write batch_script ${job_id} ${slurm_dir}/slurm-${job_id}_args.sh
+
 
 ssh -tt jjia@nodelogin02 /bin/bash << ENDSSH
 echo "Hello, I an in nodelogin02 to do some git operations."
@@ -32,7 +34,7 @@ ENDSSH
 echo "Hello, I am back in $(hostname) to run the code"
 
 # shellcheck disable=SC2046
-idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u run.py 2>${slurm_dir}/slurm-${job_id}_$idx.err 1>${slurm_dir}/slurm-${job_id}_$idx.out --outfile=${slurm_dir}/slurm-${job_id}_$idx --hostname="$(hostname)" --jobid=${job_id} --model_names="liver-pancreas" --cond_flag=True --remark="no amp"
+idx=0; export CUDA_VISIBLE_DEVICES=$idx; stdbuf -oL python -u run.py 2>${slurm_dir}/slurm-${job_id}_$idx.err 1>${slurm_dir}/slurm-${job_id}_$idx.out --outfile=${slurm_dir}/slurm-${job_id}_$idx --hostname="$(hostname)" --jobid=${job_id} --model_names="liver-pancreas" --cond_flag=False --remark="no amp"
 
 
 
